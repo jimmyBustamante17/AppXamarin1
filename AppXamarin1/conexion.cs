a95e2345e5b101238976d303d83bfef413425575
ESTUDIANTE <ESTUDIANTE@DESKTOP-7DEF2GQ>
2023-02-18 13:06:09 -0500

    Agregar archivos de proyecto.

---
+++ AppXamarin1/conexion.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppXamarin1 { 
+
+    public class Login {
+    }
+
+    public class Ticket { 
+    }
+
+    internal class Auxiliar
+    {
+        static object locker = new object();
+        SQLiteConnection conexion;
+
+        public Auxiliar (){
+
+            conexion = Conectar();
+            conexion.CreateTable<Login>();
+        }
+
+        public SQLite.SQLiteConnection Conectar()
+            
+            
+
+    }
+}
